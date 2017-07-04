5bad193a6fa90daf2dda65c5b1d8d9d8654295da
Brendan Brown <brendanbrown27@gmail.com>
2017-07-04 12:44:46 -0400

    added getRepsById and getSensById, move mock json to test folder

--- handler.js
+++ handler.js
@@ -3,51 +3,97 @@
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.hello = function(event, context, callback) {
+/*
+    Route:    /zips/:zipcode
+    Example:  /zips/91011
+*/
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ "message": "Hello World!" })
-  };
-
-  callback(null, response);
-
-};
-
-module.exports.getZip = (event, context, callback) => {
-
+function getZip (event, context, callback) {
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
+    TableName: process.env.TABLE_ZIPS,
     Key: {
       zipcode: parseInt(event.pathParameters.zipcode)
     }
   };
 
   dynamoDb.get(params, (error, result) => {
-
-    if (error) {
-      let message = 'Failed to fetch item "' + event.pathParameters.zipcode + '" from ' + params.TableName;
-      callback(new Error(message));
-      return;
-    }
-
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*", // CORS
-        "Access-Control-Allow-Credentials" : true // Cookies and authorization headers with HTTPS
-      },
-      body: JSON.stringify(zipcodeToString(result.Item))
-    };
-
-    callback(null, response);
+    if (error)
+      return _errorHandler(event.pathParameters.zipcode, params.TableName, callback);
+    else
+      return _successHandler(result.Item, callback, _zipcodeToString);
   });
 }
 
-function zipcodeToString(item) {
+/*
+    Route:    /reps/:id
+    Example:  /reps/CA-27
+*/
+
+function getRepById (event, context, callback) {
+  const params = {
+    TableName: process.env.TABLE_REPS,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error)
+      return _errorHandler(event.pathParameters.id, params.TableName, callback);
+    else
+      return _successHandler(result.Item, callback);
+  });
+}
+/*
+    Route:    /sens/:id
+    Example:  /sens/CA-1
+*/
+
+function getSenById (event, context, callback) {
+  const params = {
+    TableName: process.env.TABLE_SENS,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error)
+      return _errorHandler(event.pathParameters.id, params.TableName, callback);
+    else
+      return _successHandler(result.Item, callback);
+  });
+}
+
+
+// private functions
+
+function _errorHandler (param, table, callback) {
+  let message = 'Failed to fetch item "' + param + '" from ' + table;
+  callback(new Error(message));
+}
+
+function _successHandler (item, callback, jsonModifier) {
+  let data = jsonModifier ? jsonModifier(item) : item;
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*", // CORS
+      "Access-Control-Allow-Credentials" : true // Cookies and authorization headers with HTTPS
+    },
+    body: JSON.stringify(data)
+  };
+  callback(null, response);
+}
+
+function _zipcodeToString(item) {
   item.zipcode = item.zipcode.toString();
   if (item.zipcode.length == 4) {
     item.zipcode = '0' + item.zipcode;
   }
   return item;
 }
+
+module.exports.getZip = getZip
+module.exports.getRepById = getRepById
+module.exports.getSenById = getSenById
